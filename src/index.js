ab8721e257cfd60337f2b4e4aff26fff03c452ec
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-03-07 15:26:12 +0500

    jsx and components (nested)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This is a component
+// Stateless functional component
+// Always return JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return <h2>1.This is my first component</h2>;
+}
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Hello World")
+//   );
+// };
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
+
+// ---------------------------------------------------------------------------------------------
+
+// JSX Rules:
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+function Article() {
+  return (
+    // Implies that no other element can be returned (except for a fragment <></>)
+    <article>
+      <div>
+        <h1>2.JSX</h1>
+        <p>Paragraph</p>
+      </div>
+      <input type="text" name="" id="" />
+      <img src="" alt="" />
+    </article>
+  );
+}
+
+ReactDOM.render(<Article />, document.getElementById("article"));
+
+// ---------------------------------------------------------------------------------------------
+
+// Nested Components, React Tools
+function Nested() {
+  return (
+    <div>
+      <Message />
+      <Person />
+    </div>
+  );
+}
+
+const Person = () => <h3>Fakher Zaman</h3>;
+const Message = () => {
+  return (
+    <>
+      <h2>3.Nested Components</h2>
+      <p>Nested Component are more used in react.js for flexibility</p>
+    </>
+  );
+};
+
+ReactDOM.render(<Nested />, document.getElementById("nested"));
---
+++ src/testing.js
@@ -0,0 +1,8 @@
+// rfce
+import React from "react";
+
+function testing() {
+  return <div>testing</div>;
+}
+
+export default testing;
