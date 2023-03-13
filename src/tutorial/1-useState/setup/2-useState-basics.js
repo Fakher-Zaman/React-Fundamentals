93d349cd845be7cc5cd0d1025ed5195a45be310d
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-03-13 16:04:17 +0500

    General Rules of Hooks

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
+// useState is a function
+// call useState() -> this returns a value
+// destructure this value
+// useState('hello world') -> ['hello world', f]
+// component needs to re-render
+
+// ===========Rules of Hooks (useState, useEffect, useRef, etc)==========
+// 1. must be in the function/component body
+// 2. must be called at the top level
+// 3. cannot call conditionally
+// 4. cannot call inside loops, nested functions, or anything that isn't a function
+// 6. components must start with a capital letter
 
 const UseStateBasics = () => {
   // console.log(useState('hello world'));
   // const value = useState(1)[0];
   // const handler = useState(1)[1];
   // console.log(value, handler);
-  const [text, setText] = useState('random title');
+  const [text, setText] = useState('random title'); // Hook
   return <React.Fragment>
     <h1>{text}</h1>
     <button className="btn" onClick={() => (text === 'random title') ? setText('hello world') : setText('random title')}>change title</button>
