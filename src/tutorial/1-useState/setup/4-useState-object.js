d5a74f295771c365e7c3d98d959e8df8e8f277d5
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-03-13 17:12:54 +0500

    useState - Multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,12 +6,24 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   });
+
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random message');
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'hello world' });
+    setName('john');
+    setAge(25);
+    setMessage('hello world');
+  };
+
   return (
     <>
       <h3>{person.name}</h3>
       <h3>{person.age}</h3>
       <h3>{person.message}</h3>
-      <button className="btn" onClick={() => setPerson({ ...person, message: 'hello world' })}>
+      <button className="btn" onClick={changeMessage}>
         change message
       </button>
     </>
