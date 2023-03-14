64af413c22d2a3dc918b0b228e344d196b573116
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-03-14 15:27:59 +0500

    useEffect Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Final from './tutorial/1-useState/final/3-useState-array';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
   return (
     <div className="container">
-      <Final />
+      <Setup />
     </div>
   );
 }
