e72d544bc92d83bf2aeb6d774255a8900d7fbdb9
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-03-14 19:59:54 +0500

    Multiple Returns - Fetching Data

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -6,6 +6,26 @@ const MultipleReturns = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState('default user');
 
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   if (isLoading) {
     return (
       <div>
