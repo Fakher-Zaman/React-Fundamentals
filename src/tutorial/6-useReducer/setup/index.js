cfa28615e52488371ebd942cc8284d07b172e12e
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-01 20:50:30 +0500

    useReducer - add item

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,22 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
 
+// reducer functionb
 const reducer = (state, action) => {
-
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'item added',
+    };
+  }
+  if (action.type === 'NO_VALUE') {
+    return { ...state, isModalOpen: true, modalContent: 'please enter value' };
+  }
+  throw new Error('no matching action type');
 }
 
 const defaultState = {
@@ -19,9 +31,11 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: 'ADD_ITEM', payload: newItem });
+      setName('');
     } else {
-
+      dispatch({ type: 'NO_VALUE' });
     }
   };
 
