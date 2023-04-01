e494f10baf0f3f06e67ab3251db9bfd8909c341b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-01 19:44:51 +0500

    useReducer - Refactor

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 
-const Modal = () => {
-  return <div>i'm modal</div>;
+const Modal = ({ modelContent }) => {
+  return (
+    <div className='modal'>
+      <p>{modelContent}</p>
+    </div>
+  );
 };
 
 export default Modal;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,24 +3,31 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
+const reducer = (state, action) => {
+
+}
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+}
+
 const Index = () => {
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
-      setName('');
+
     } else {
-      setShowModal(true);
+
     }
   };
 
   return (
     <>
-      {showModal && <Modal />}
+      {state.showModal && <Modal modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
@@ -31,7 +38,7 @@ const Index = () => {
         </div>
         <button type='submit'>add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
