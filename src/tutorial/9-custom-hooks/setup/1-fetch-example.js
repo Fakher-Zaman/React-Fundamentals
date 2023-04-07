eb3e8d0ee56751d645f3d6678321db607f619f68
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-07 16:36:38 +0500

    Custom Hooks - useFetch

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,24 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './2-useFetch'
+import React, { useState, useEffect } from 'react';
+import { useFetch } from './2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
+  const { loading, products } = useFetch(url);  // useFetch() is a custom hook
 
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
   console.log(products)
   return (
     <div>
