064ac410df19d8c538f24e5ffc4bc004656574ce
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-07 17:46:43 +0500

    ProtoTypes - Default Values

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -3,11 +3,11 @@ import ProtoTypes from 'prop-types';
 import defaultImage from '../../../assets/default-image.jpeg';
 
 const Product = ({ image, name, price }) => {
-  console.log(image, name, price);
+  const url = image && image.url;
   return <article className='product'>
-    <img src={image.url} alt={name} />
+    <img src={url || defaultImage} alt={name || 'default name'} />
     <h4>{name}</h4>
-    <p>${price}</p>
+    <p>${price || 3.99}</p>
   </article>;
 };
 
@@ -17,10 +17,10 @@ Product.ProtoTypes = {
   price: ProtoTypes.number.isRequired,
 };
 
-Product.defaultProps = {
-  name: 'default name',
-  price: 3.99,
-  image: defaultImage
-}
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage
+// }
 
 export default Product;
---
+++ src/tutorial/10-prop-types/setup/testing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProtoTypes from 'prop-types';
+
+const testing = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+testing.ProtoTypes = {
+    // name:PropTypes.array.isRequired,
+}
+
+export default testing
