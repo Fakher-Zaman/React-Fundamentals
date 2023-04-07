d13faf61dc63b661f050f9d1bce64594c83eabd2
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-07 16:57:11 +0500

    ProtoTypes - Setup

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  console.log(image, name, price);
+  return <article className='product'>
+    <h4>single product</h4>
+    {/* <img src={image.url} alt={name} />
+    <h4>{name}</h4>
+    <p>${price}</p> */}
+  </article>;
 };
 
 export default Product;
--- src/tutorial/10-prop-types/setup/index.js
+++ src/tutorial/10-prop-types/setup/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Product from './Product'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React from 'react';
+import Product from './Product';
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-prop-types-example'
+const url = 'https://course-api.com/react-prop-types-example';
 
 const Index = () => {
-  const { products } = useFetch(url)
+  const { products } = useFetch(url);
   return (
     <div>
       <h2>products</h2>
@@ -20,4 +20,4 @@ const Index = () => {
   )
 }
 
-export default Index
+export default Index;
