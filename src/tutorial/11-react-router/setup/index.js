bcc20b8969244ea47742c1f5c16c1a8288975a68
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-08 14:38:00 +0500

    React Router - Error And Switch Component

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -11,15 +11,20 @@ import Person from './Person';
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
   return <Router>
-    <Route exact path="/">
-      <Home />
-    </Route>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path="/people">
-      <People />
-    </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/people">
+        <People />
+      </Route>
+      <Route path="*">
+        <Error />
+      </Route>
+    </Switch>
   </Router>;
 };
 
