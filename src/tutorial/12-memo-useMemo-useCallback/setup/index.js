5b5b4c2397ef1a7dcdb7134b04cd8519b7bb1646
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-04-08 17:37:15 +0500

    useMemo

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -3,9 +3,20 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
-
+const url = 'https://course-api.com/javascript-store-products';
 // every time props or state changes, component re-renders
+const calculateMostExpensive = (data) => {
+  console.log('hello');
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price >= total) {
+        total = price;
+      }
+      return total;
+    }, 0) / 100
+  );
+};
 
 const Index = () => {
   const { products } = useFetch(url);
@@ -16,6 +27,10 @@ const Index = () => {
     setCart(cart + 1);
   }, [cart]);
 
+  const mostExpensive = useMemo(() => calculateMostExpensive(products), [
+    products,
+  ]);
+
   return (
     <>
       <h1>Count : {count}</h1>
@@ -23,6 +38,7 @@ const Index = () => {
         click me
       </button>
       <h1 style={{ marginTop: '3rem' }}>cart : {cart}</h1>
+      <h1>Most Expensive : ${mostExpensive}</h1>
       <BigList products={products} addToCart={addToCart} />
     </>
   )
